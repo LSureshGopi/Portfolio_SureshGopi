c05fc7a19fee0353df425275f2a09536879d6a8f
loli33 <sugundanmm33@email.com>
2025-05-19 17:05:37 +0530

    first commit

---
+++ static/script.js
@@ -0,0 +1,10 @@
+// Example of Smooth Scroll for internal navigation (Optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
